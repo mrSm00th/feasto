b09b176bedbf697273f5cca2e22a9242ff41a3d3
mrSm00th <231942191+mrSm00th@users.noreply.github.com>
2026-05-20 16:55:21 +0530

    feat(owner-app): add get owner application by id endpoint

--- app/modules/owner_applications/router.py
+++ app/modules/owner_applications/router.py
@@ -1,7 +1,8 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy import select
+from fastapi import APIRouter, Depends, HTTPException, status, Query
+from sqlalchemy import select, func
+from sqlalchemy.orm import selectinload
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -11,24 +12,32 @@ from app.modules.owner_applications.models import OwnerApplication
 from app.modules.owner_applications.schemas import (
     OwnerApplicationCreate,
     OwnerApplicationResponse,
+    PaginatedOwnerAppResponse,
+    OwnerApplicationMini,
 )
-from app.modules.users.models import UserRole
-
-router = APIRouter(prefix="/api/partner", tags=["owner applications"])
+from app.modules.users.models import UserRole, User
+from app.modules.admin.schemas import OwnerApplicationDetailed
+from app.core.config import settings
+import uuid
+router = APIRouter(prefix="/api/owner-applications", tags=["owner applications"])
 
 
 @router.post(
-    "/application",
+    "/applications",
     response_model=OwnerApplicationResponse,
     status_code=status.HTTP_201_CREATED,
 )
-async def create_application(
+async def create_owner_application(
     data: OwnerApplicationCreate,
     db: Annotated[AsyncSession, Depends(get_db)],
-    current_user=Depends(require_roles(UserRole.CUSTOMER)),
+    current_user: Annotated[User, Depends(require_roles(UserRole.CUSTOMER))],
 ):
+    
+
     result = await db.execute(
-        select(OwnerApplication).where(
+        select(OwnerApplication)
+        .options(selectinload(OwnerApplication.applicant))
+        .where(
             OwnerApplication.applicant_id == current_user.id,
             OwnerApplication.status == "PENDING",
         )
@@ -57,3 +66,85 @@ async def create_application(
         raise HTTPException(status_code=400, detail="FSSAI number already exists")
 
     return new_application
+
+#get all the user applications
+@router.get(
+    "/applications",
+    response_model= PaginatedOwnerAppResponse,
+)
+async def get_owner_applications_paginated(
+    current_user: Annotated[User, Depends(require_roles(UserRole.CUSTOMER, UserRole.RESTAURANT_OWNER))],
+    db: Annotated[AsyncSession, Depends(get_db)],
+    skip: Annotated[int, Query(ge=0)] = 0,
+    limit: Annotated[int, Query(ge=1, le=100)] = settings.application_per_page
+):
+    
+    result_count = await db.execute(
+        select(func.count())
+        .select_from(OwnerApplication)
+        .where(OwnerApplication.applicant_id == current_user.id)
+    )
+
+    total = result_count.scalar() or 0
+
+    result = await db.execute(
+        select(OwnerApplication)
+        .where(OwnerApplication.applicant_id == current_user.id)
+        .order_by(OwnerApplication.created_at.desc())
+        .offset(skip)
+        .limit(limit)
+    )
+
+    applications = result.scalars().all()
+
+    has_more = skip + len(applications) < total
+
+    return PaginatedOwnerAppResponse(
+        applications=[
+            OwnerApplicationMini.model_validate(application)
+            for application in applications
+        ],
+        total=total,
+        skip=skip,
+        limit=limit,
+        has_more=has_more,
+    )
+
+
+# get the particular apllication of the owner by app_id
+@router.get(
+    "/applications/{id}",
+    response_model= OwnerApplicationDetailed,
+)
+async def get_owner_application_by_id(
+        id: uuid.UUID,
+        db: Annotated[AsyncSession, Depends(get_db)],
+        current_user: Annotated[User, Depends(require_roles(UserRole.CUSTOMER, UserRole.RESTAURANT_OWNER))]
+):
+    
+    result = await db.execute(
+        select(OwnerApplication)
+        .where(OwnerApplication.id == id)
+    )
+
+    application = result.scalars().first()
+
+    if not application:
+
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "Application not found",
+        )
+
+    if application.applicant_id != current_user.id:
+
+        raise HTTPException(
+            status_code = status.HTTP_403_FORBIDDEN,
+            detail = "You don't have permission to access this application",
+        )
+    
+
+    return application
+    
+    
+    
--- app/modules/owner_applications/schemas.py
+++ app/modules/owner_applications/schemas.py
@@ -4,6 +4,7 @@ from typing import Annotated
 from pydantic import BaseModel, ConfigDict, Field
 
 from app.modules.owner_applications.models import ApplicationStatus
+from datetime import datetime
 
 
 class BaseSchema(BaseModel):
@@ -15,20 +16,26 @@ class OwnerApplicationCreate(BaseSchema):
     restaurant_name: Annotated[str, Field(min_length=3, max_length=100)]
     fssai_license_number: Annotated[str, Field(min_length=14, max_length=14)]
     gst_number: Annotated[str, Field(min_length=15, max_length=15)]
-    # pan_number: Annotated[str, Field(min_length=10, max_length=10)]
-    # bank_account_number: Annotated[str, Field(min_length=15, max_length=20)]
-
 
 class OwnerApplicationResponse(
     OwnerApplicationCreate
 ):  # only sending the response to the owner
 
-    # restaurant_name: Annotated[str, Field(min_length=3, max_length=100)]
-    # fssai_license_number: Annotated[str, Field(min_length=14, max_length=14)]
-    # gst_number: Annotated[str, Field(min_length=15, max_length= 15)]
-
-    # pan_number: Annotated[str, Field(min_length=10, max_length=10)]
-    # bank_account_number: Annotated[str, Field(min_length=15, max_length=20)]
     id: uuid.UUID
     status: ApplicationStatus
     rejection_reason: Annotated[str | None, Field(max_length=500)]
+
+class OwnerApplicationMini(BaseSchema):
+
+    id: uuid.UUID
+    restaurant_name: str
+    status: ApplicationStatus
+    created_at: datetime
+
+
+class PaginatedOwnerAppResponse(BaseSchema):
+    applications: list[OwnerApplicationMini]
+    total: int
+    skip: int
+    limit: int
+    has_more: bool
